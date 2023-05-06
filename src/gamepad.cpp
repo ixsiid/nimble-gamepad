8edc772b1437c9f76e66a10e097562e2ea9ee7f0
ixsiid <ixsiid@halzion.net>
2023-05-06 20:35:54 +0900

    namespaceを使って整理

--- src/gamepad.cpp
+++ src/gamepad.cpp
@@ -7,9 +7,9 @@ using namespace SimpleNimble;
 const char *BleGamePad::tag = "BleGamePad";
 
 // model, serial, firm, soft, hard は必須ではない
-static SimpleNimbleCharacteristicBuffer *model, *serial, *firm, *soft, *hard, *level, *manu;
+static Characteristic *model, *serial, *firm, *soft, *hard, *level, *manu;
 
-static SimpleNimbleCharacteristicBuffer *hid_info, *hid_control, *hid_report_map, *hid_proto, *hid_report_pad0, *hid_pnp;
+static Characteristic *hid_info, *hid_control, *hid_report_map, *hid_proto, *hid_report_pad0, *hid_pnp;
 
 static Descriptor pad0;
 
@@ -46,28 +46,28 @@ BleGamePad::BleGamePad(const char *device_name)
 	battery_report.data_length	 = sizeof(present_format_t);
 	present_format_t *pft		 = (present_format_t *)battery_report.buffer;
 	// battery level: 32%
-	level = new SimpleNimbleCharacteristicBuffer(0x2a19, 1,
-										Chr_AccessFlag::Read | Chr_AccessFlag::Notify,
-										{&battery_report});
+	level = new Characteristic(0x2a19, 1,
+						  Chr_AccessFlag::Read | Chr_AccessFlag::Notify,
+						  {&battery_report});
 	level->write_u8(32);
 
 	// model revision
-	model = new SimpleNimbleCharacteristicBuffer(0x2a24, 8, Chr_AccessFlag::Read);
+	model = new Characteristic(0x2a24, 8, Chr_AccessFlag::Read);
 	model->write("1.0.0");
 	// serial number
-	serial = new SimpleNimbleCharacteristicBuffer(0x2a25, 8, Chr_AccessFlag::Read);
+	serial = new Characteristic(0x2a25, 8, Chr_AccessFlag::Read);
 	serial->write("12345");
 	// firmware version
-	firm = new SimpleNimbleCharacteristicBuffer(0x2a26, 8, Chr_AccessFlag::Read);
+	firm = new Characteristic(0x2a26, 8, Chr_AccessFlag::Read);
 	firm->write("0.1.0");
 	// hardware version
-	hard = new SimpleNimbleCharacteristicBuffer(0x2a27, 8, Chr_AccessFlag::Read);
+	hard = new Characteristic(0x2a27, 8, Chr_AccessFlag::Read);
 	hard->write("0.2.1");
 	// software version
-	soft = new SimpleNimbleCharacteristicBuffer(0x2a28, 8, Chr_AccessFlag::Read);
+	soft = new Characteristic(0x2a28, 8, Chr_AccessFlag::Read);
 	soft->write("0.3.2");
 	// manufacture
-	manu = new SimpleNimbleCharacteristicBuffer(0x2a29, 8, Chr_AccessFlag::Read);
+	manu = new Characteristic(0x2a29, 8, Chr_AccessFlag::Read);
 	manu->write("ixsiid");
 
 	// device information
@@ -79,17 +79,17 @@ BleGamePad::BleGamePad(const char *device_name)
 	ESP_LOGI(tag, "add battery service: %d", r);
 
 	// u32, u16でwriteするときはuint8_t[]とは逆順になるので注意
-	hid_info = new SimpleNimbleCharacteristicBuffer(0x2a4a, 4, Chr_AccessFlag::Read);
+	hid_info = new Characteristic(0x2a4a, 4, Chr_AccessFlag::Read);
 	// HID verion Low 0x11, HID version High 0x01, country code 0x00, HID_FLAGS_NORMALLY_CONNECTABLE 0x02
 	hid_info->write_u32(0x02000111);
 
-	hid_control = new SimpleNimbleCharacteristicBuffer(0x2a4c, 1, Chr_AccessFlag::Write_no_response);
+	hid_control = new Characteristic(0x2a4c, 1, Chr_AccessFlag::Write_no_response);
 	hid_control->write_u8(0);
 
-	hid_report_map = new SimpleNimbleCharacteristicBuffer(0x2a4b, sizeof(hid_report_map_data), Chr_AccessFlag::Read);
+	hid_report_map = new Characteristic(0x2a4b, sizeof(hid_report_map_data), Chr_AccessFlag::Read);
 	hid_report_map->write(hid_report_map_data, sizeof(hid_report_map_data));
 
-	hid_proto = new SimpleNimbleCharacteristicBuffer(0x2a4e, 7, Chr_AccessFlag::Read | Chr_AccessFlag::Write_no_response);
+	hid_proto = new Characteristic(0x2a4e, 7, Chr_AccessFlag::Read | Chr_AccessFlag::Write_no_response);
 	// HID_PROTOCOL_MODE_REPORT
 	hid_proto->write_u8(1);
 
@@ -102,13 +102,13 @@ BleGamePad::BleGamePad(const char *device_name)
 	pad0.buffer[0]		 = 0x01;
 	pad0.buffer[1]		 = 0x01;
 
-	hid_report_pad0	= new SimpleNimbleCharacteristicBuffer(0x2a4d, sizeof(gamepad_t), Chr_AccessFlag::Read | Chr_AccessFlag::Notify, {&pad0});
+	hid_report_pad0	= new Characteristic(0x2a4d, sizeof(gamepad_t), Chr_AccessFlag::Read | Chr_AccessFlag::Notify, {&pad0});
 	_buffer.pad.slider	= 0x1234;
 	_buffer.pad.x		= 0x5432;
 	_buffer.pad.buttons = 0b10100101;
 	hid_report_pad0->write(_buffer.raw, (uint8_t)sizeof(gamepad_t));
 
-	hid_pnp = new SimpleNimbleCharacteristicBuffer(0x2a50, 7, Chr_AccessFlag::Read);
+	hid_pnp = new Characteristic(0x2a50, 7, Chr_AccessFlag::Read);
 	// Bluetooth SIG compani: 0xe502, Product Id: 52651 (0xcdab), Product Version 4097 (0x1001)
 	hid_pnp->write({0x01, 0x02, 0xe5, 0xab, 0xcd, 0x01, 0x10});
 
